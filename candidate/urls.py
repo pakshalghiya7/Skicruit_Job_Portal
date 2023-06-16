ce7da2f69af27374b20ba9aee9ee6fe6fb0def81
Pakshal Ghiya <pakshal@simformsolutions.com>
2023-06-16 19:08:54 +0530

    Home Page Added

--- candidate/urls.py
+++ candidate/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('', views.home, name='home'),
     path('', views.home_Candidate, name='candidate-Home'),
     path('profile/', views.my_profile, name='my-profile'),
     path('profile/edit/', views.edit_profile, name='edit-profile'),
--- candidate/views.py
+++ candidate/views.py
@@ -8,8 +8,11 @@ from .forms import ProfileUpdateForm, SkillUpdateForm
 # from django import form
 from django.contrib.auth import get_user_model
 
-
 # Create your views here.
+
+def home(request):
+    return render(request,"candidate/home.html")
+
 def home_Candidate(request):
     context = {"home_page": 'active'}
     return render(request, "candidate/candidate_Home.html", context)
@@ -21,7 +24,7 @@ def my_profile(request):
     you = request.user  # Not User
     profile = Profile.objects.filter(user=you).first()
     skills = userSkill.objects.filter(user_skills=you)
-    print(profile)
+    print(profile)   ### 
     if request.method == 'POST':
         form = SkillUpdateForm(request.POST)
         if form.is_valid():
