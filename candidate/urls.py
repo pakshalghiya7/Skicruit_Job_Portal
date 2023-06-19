90312383e48b38ae996d716ef1b047ecc5e9b362
Pakshal Ghiya <pakshal@simformsolutions.com>
2023-06-19 18:56:02 +0530

    Job Searchand Details added

--- candidate/urls.py
+++ candidate/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
     path('', views.home_Candidate, name='candidate-Home'),
     path('profile/', views.my_profile, name='my-profile'),
     path('profile/edit/', views.edit_profile, name='edit-profile'),
+    path('job/', views.job_search_list, name='job-search-list'),
     
 ]
 
--- candidate/views.py
+++ candidate/views.py
@@ -7,11 +7,14 @@ from .models import Profile, Skill, userSkill
 from .forms import ProfileUpdateForm, SkillUpdateForm
 # from django import form
 from django.contrib.auth import get_user_model
+from recruiter.models import Job
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 
 # Create your views here.
 
 def home(request):
-    return render(request,"candidate/home.html")
+    return render(request, "candidate/home.html")
+
 
 def home_Candidate(request):
     context = {"home_page": 'active'}
@@ -24,7 +27,7 @@ def my_profile(request):
     you = request.user  # Not User
     profile = Profile.objects.filter(user=you).first()
     skills = userSkill.objects.filter(user_skills=you)
-    print(profile)   ### 
+    print(profile)
     if request.method == 'POST':
         form = SkillUpdateForm(request.POST)
         if form.is_valid():
@@ -79,3 +82,65 @@ def profile_view_for_recruiter(request, pk):  # Think about AutoSlug
 
     }
     return render(request, "cabdidate/   .html", context)
+
+
+@login_required
+def job_search_list(request):
+    search_query = request.GET.get("query")
+    search_location = request.GET.get("location")
+    query_list=[]
+    if (search_query == None):  # For Location ...And,Or
+        job = Job.objects.all()
+    else:
+        find_title = Job.objects.filter(title__icontains=search_query).order_by(
+            "-posted_At")  # Updated_At ,values
+        find_company = Job.objects.filter(
+            company__icontains=search_query).order_by("-posted_At")
+        find_type = Job.objects.filter(
+            type__icontains=search_query).order_by("-posted_At")
+        find_skills = Job.objects.filter(
+            skills_required__icontains=search_query).order_by("-posted_At")
+        
+        for i in find_title:
+            query_list.append(i)
+        for i in find_company:
+            query_list.append(i)
+        for i in find_type:
+            query_list.append(i)
+        for i in find_skills:
+            query_list.append(i)
+
+    if (search_location == None):    #Make Model Changes --> City
+        locat=Job.objects.all()
+    else:
+        locat=Job.objects.filter(country__icontains=search_location).order_by("-posted_At")
+    
+
+    final_query_list=[]
+    for i in query_list:
+        if i in locat:
+            final_query_list.append(i)
+
+   
+    # paginator = Paginator(final_query_list, 1)
+    # page=request.GET.get("page")
+    # # try:
+    # page_number = paginator.page(page)
+    # page_obj = paginator.get_page(page_number)
+    # except PageNotAnInteger:
+    #     page_number = paginator.page(1)
+    # except EmptyPage:
+    #     page_number = paginator.page(paginator.num_pages)
+    paginator = Paginator(final_query_list, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'jobs': page_obj,
+        'query': search_query,
+    }
+    return render(request, 'candidate/job_search_list.html', context)
+
+
+
+
+        
