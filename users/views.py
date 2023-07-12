02c7d25ba59035f6e0b406c4730696d46e246fa2
Pakshal Ghiya <pakshal@simformsolutions.com>
2023-07-12 10:54:54 +0530

    Uses Changes

--- users/views.py
+++ users/views.py
@@ -43,11 +43,12 @@ def loginview(request):
         user = authenticate(request, email=email, password=password)
         print(user.is_employee)
 
-        if user.is_employee :
+        if user.is_employee == True :
 
             login(request, user)
             return redirect('home1')
         elif user.is_employee is False:
+            login(request, user)
             return redirect('detail-recruiters')
         else:
             return render(request, 'users/login.html')  # Message
