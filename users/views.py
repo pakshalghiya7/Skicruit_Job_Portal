2f9c00b0592b043964beab33aa4809d5c68eb1ce
Pakshal Ghiya <pakshal@simformsolutions.com>
2023-07-13 12:32:27 +0530

    Message added On Invalid Login

--- users/views.py
+++ users/views.py
@@ -2,6 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, HttpResponse
 from .models import CustomUser
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 
 # Create your views here.
@@ -35,22 +40,35 @@ def signup(request):
     return render(request, 'users/signup.html', context)
 
 
-def loginview(request):
+def loginview(request):    
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
 
         user = authenticate(request, email=email, password=password)
-        print(user.is_employee)
+        # print(user)
+        if user is None:
+            messages.error(request, 'Invalid login credentials. Please try again.')
 
-        if user.is_employee == True :
+            return render(request, 'users/login.html')  # Message
+
+        if user.is_employee is True :
 
             login(request, user)
             return redirect('home1')
         elif user.is_employee is False:
             login(request, user)
             return redirect('detail-recruiters')
-        else:
-            return render(request, 'users/login.html')  # Message
-
+      
     return render(request, 'users/login.html')
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    subject_template_name = 'users/password_reset_subject'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('users-home')
